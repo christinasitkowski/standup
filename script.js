e0a556ceed1ffaea10e26f14368f575f570c90ba
Christina Sitkowski <christina@celtx.com>
2023-11-10 11:25:16 -0600

    Add files for stand up tracker app

---
+++ script.js
@@ -0,0 +1,49 @@
+const names = [
+  "Andrew",
+  "Christina",
+  "D.J.",
+  "Lauren",
+  "Marcel",
+  "Matthew"
+];
+
+let count = names.length;
+
+let first = Math.floor(Math.random() * count);
+
+names.sort(() => Math.random() - 0.5);
+
+const container = document.querySelector("#name-blocks");
+
+names.forEach((name, index) => {
+  
+  let div = document.createElement("div");
+  div.className = "name-block";
+  let el = document.createTextNode(name);
+
+  div.appendChild(el);
+  container.appendChild(div);
+  
+  if (index == first) {
+    let badge = document.createElement("div");
+    badge.className = "badge";
+    div.className = "name-block animate__animated animate__pulse animate__repeat-3";
+    div.appendChild(badge);
+  }
+
+  div.addEventListener("click", () => {
+    if (div.classList.contains("disabled")) {
+      div.classList.remove("disabled");
+      count++;
+    } else {
+      div.classList.add("disabled");
+      count--;
+    }
+
+    if (count == 0) {
+      confetti.start();
+    } else {
+      confetti.stop();
+    }
+  });
+});
